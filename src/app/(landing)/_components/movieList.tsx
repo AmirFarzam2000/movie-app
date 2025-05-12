2a4abbdccf36a3a953f3488ade8efe52dc810b0d
AmirFarzam2000 <amirmehdifarzamfar1380@gmail.com>
2025-05-12 08:12:16 -0700

    feat: add MovieList component to display list of movies

---
+++ src/app/(landing)/_components/movieList.tsx
@@ -0,0 +1,23 @@
+
+import { Movie } from "src/model/movie";
+import MovieCard from "./movieCard";
+
+interface MovieListProps {
+  movies: Movie[];
+}
+
+const MovieList = ({ movies }: MovieListProps) => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          id={movie.id}
+          title={movie.title}
+          posterPath={movie.poster_path} slug={""}        />
+      ))}
+    </div>
+  );
+};
+
+export default MovieList;
