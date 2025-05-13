f6a816efb9f8f400368e7f92edfb19afefca20d8
AmirFarzam2000 <amirmehdifarzamfar1380@gmail.com>
2025-05-13 03:41:15 -0700

    feat: create MovieCard component with image, title, and link to details page

--- src/app/(landing)/_components/movieCard.tsx
+++ src/app/(landing)/_components/movieCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 interface MovieCardProps {
@@ -10,13 +11,19 @@ const MovieCard = ({ id, title, posterPath }: MovieCardProps) => {
   const posterUrl = `https://image.tmdb.org/t/p/w500${posterPath}`;
 
   return (
-    <div className="bg-white p-4 rounded-md shadow-lg">
-      <img
-        src={posterUrl}
-        alt={title}
-        className="w-full h-64 object-cover rounded-md"
-      />
-      <h3 className="mt-2 text-lg font-semibold">{title}</h3>
+    <div className="bg-white p-4 rounded-md shadow-lg flex flex-col">
+      <Image
+  src={posterUrl}
+  alt={title}
+  width={300}
+  height={400}
+  unoptimized
+  className="rounded-md object-cover"
+/>
+
+      <div className="flex-1 mt-2">
+        <h3 className="text-lg sm:text-xl font-semibold line-clamp-2">{title}</h3>
+      </div>
       <Link href={`/movie/${id}-${title.replace(/\s+/g, "-").toLowerCase()}`} className="text-blue-500 mt-2 block">
         View Details
       </Link>
