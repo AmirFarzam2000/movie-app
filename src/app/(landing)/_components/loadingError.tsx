33f8a41a3e1ad8221dcfe6f30258b986dd302ba2
AmirFarzam2000 <amirmehdifarzamfar1380@gmail.com>
2025-05-13 03:44:44 -0700

    Fix bugs

--- src/app/(landing)/_components/loadingError.tsx
+++ src/app/(landing)/_components/loadingError.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 
 interface LoadingErrorProps {
-  isLoading: boolean;
+  // isLoading: boolean;
   isError: boolean;
 }
 
-const LoadingError: React.FC<LoadingErrorProps> = ({ isLoading, isError }) => {
-//   if (isLoading) return <p className="text-center"><LoadingSpinner/></p>;
+const LoadingError: React.FC<LoadingErrorProps> = ({isError }) => {
   if (isError) return <p className="text-center text-red-500">Error loading data</p>;
   return null;
 };
